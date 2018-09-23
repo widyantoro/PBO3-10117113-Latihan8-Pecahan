0525d5db53b9a09496ce9f6df74dda3c1f840cc2
Eka Widyantoro <ekawidyantoro17@gmail.com>
2018-09-23 18:40:50 +0700

    first commit

---
+++ src/pbo3/pkg10117113/latihan8/pecahan/PBO310117113Latihan8Pecahan.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3.pkg10117113.latihan8.pecahan;
+
+/*
+ * 
+ * @author
+ * Nama     : Eka Widyantoro
+ * Kelas    : IF-3 / PBO3
+ * NIM      : 10117113
+ * Deskripsi Program : Program menampilkan pecahan
+ */
+public class PBO310117113Latihan8Pecahan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        float f;
+        double d;
+        f = 1234567890.123456789F;
+        d = 1_234_567_890.123456789;
+        System.out.println("Float : " + f);
+        System.out.println("Double : " + d);
+    }
+    
+}
